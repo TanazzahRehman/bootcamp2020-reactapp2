3380cba6dddb6633a14aa8cc51ae628ded6483d5
TanazzahRehman <tanazzahrehman@gmail.com>
2020-07-25 21:04:10 +0500

    basic react state

---
+++ src/Room.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+//console.log("state=" ,state);
+
+function Room() {
+    const [islit, setlit] = useState(false);
+    let [age, setage] = useState(1);
+
+    function update(){
+        console.log("Button clicked");
+        setlit(!islit);
+    }
+
+    const incrementor = () => {
+        console.log("increase age by 1");
+        setage(++age);
+     }
+    
+  return (
+      
+    <div>
+      this room is {islit ? "lit": "dark"}
+   
+       <br /> 
+       <button onClick = {update} > Toggle light </button>
+       <br />
+       Age : {age} 
+       <br />
+       <button onClick = { () =>
+        {
+        console.log("increase age by 1");
+        setage(++age);
+       }}>
+            increment age </button>
+    </div>
+  );
+}
+
+export default Room;
